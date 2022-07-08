a76de65ec85efde938fc933da100992d9f72d5ef
Nitin Pandey <73239889+Nitinp13@users.noreply.github.com>
2022-07-08 20:11:20 +0530

    Time: 9 ms (82.57%), Space: 55 MB (22.49%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int l = 0, r = s.length()-1;
+        int count = 0;
+        while(l<=r){
+            if(s.charAt(l) != s.charAt(r)){
+                return vP(s,l+1,r) || vP(s, l, r-1);
+            }
+            l++;
+            r--;
+        }
+        return true;
+        
+    }
+    
+    private boolean vP(String s , int i , int j){
+        while(i < j){
+            if(s.charAt(i) != s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
