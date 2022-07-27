441cbaa58e1d2f9a7190f6e3490a5e456d5d7197
Nitin Pandey <73239889+Nitinp13@users.noreply.github.com>
2022-07-27 18:47:42 +0530

    Time: 1 ms (78.58%), Space: 42.9 MB (36.07%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
@@ -14,27 +14,40 @@
  * }
  */
 class Solution {
-     ArrayList<Integer> list = new ArrayList<>();
+    // Better solution
+    private TreeNode prev = null;
+
+public void flatten(TreeNode root) {
+    if (root == null)
+        return;
+    flatten(root.right);
+    flatten(root.left);
+    root.right = prev;
+    root.left = null;
+    prev = root;
+}
+    // Naive Solution
+//      ArrayList<Integer> list = new ArrayList<>();
     
-    public void flatten(TreeNode root) {
-        if(root == null) return;
+//     public void flatten(TreeNode root) {
+//         if(root == null) return;
         
-        helper(root);
-        root.left = null;
-        // root.right = null;
-        for(int i = 1;i<list.size();i++){
-            root.right = new TreeNode(list.get(i));
-            root = root.right;
-        }
+//         helper(root);
+//         root.left = null;
+//         // root.right = null;
+//         for(int i = 1;i<list.size();i++){
+//             root.right = new TreeNode(list.get(i));
+//             root = root.right;
+//         }
         
-    }
-    public void helper(TreeNode root){
-        if(root == null){
-            return;
-        }
-        list.add(root.val);
+//     }
+//     public void helper(TreeNode root){
+//         if(root == null){
+//             return;
+//         }
+//         list.add(root.val);
         
-        helper(root.left);
-        helper(root.right);
-    }
+//         helper(root.left);
+//         helper(root.right);
+//     }
 }
