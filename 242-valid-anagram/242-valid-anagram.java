602c99f3b4433743a004377451e74ddcfbd1e13a
Nitin Pandey <73239889+Nitinp13@users.noreply.github.com>
2022-07-28 21:53:08 +0530

    Time: 5 ms (75.47%), Space: 43.4 MB (74.06%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,25 +1,35 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()) return false;
-        HashMap<Character, Integer> hm = new HashMap<>();
-        for(int i = 0;i<s.length();i++){
-            char c = s.charAt(i);
-            hm.put(c, hm.getOrDefault(c, 0) + 1);
-            
-        }
-        for(int i = 0;i<t.length();i++){
-            char c = t.charAt(i);
-            
-            if(hm.containsKey(c) == false){
-                return false;
-            }else if(hm.get(c) == 1){
-                hm.remove(c);
-            }else{
-                 hm.put(c, hm.get(c) - 1);
-            }
-        }
+        int[] alp = new int[26];
+    for (int i = 0; i < s.length(); i++) 
+        alp[s.charAt(i) - 'a']++;
+    for (int i = 0; i < t.length(); i++)
+        alp[t.charAt(i) - 'a']--;
+    for (int i=0;i<26;i++)
+        if (alp[i] != 0) 
+            return false;
+        return true;
         
-        return hm.size() == 0;
+//         if(s.length() != t.length()) return false;
+//         HashMap<Character, Integer> hm = new HashMap<>();
+//         for(int i = 0;i<s.length();i++){
+//             char c = s.charAt(i);
+//             hm.put(c, hm.getOrDefault(c, 0) + 1);
+            
+//         }
+//         for(int i = 0;i<t.length();i++){
+//             char c = t.charAt(i);
+            
+//             if(hm.containsKey(c) == false){
+//                 return false;
+//             }else if(hm.get(c) == 1){
+//                 hm.remove(c);
+//             }else{
+//                  hm.put(c, hm.get(c) - 1);
+//             }
+//         }
+        
+//         return hm.size() == 0;
         
     }
 }
