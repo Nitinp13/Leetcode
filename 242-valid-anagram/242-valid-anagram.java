787a9509e86506aae9b1facb60bd6cc0231f6791
Nitin Pandey <73239889+Nitinp13@users.noreply.github.com>
2022-07-28 20:27:16 +0530

    Time: 26 ms (18.57%), Space: 46.3 MB (23.74%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -4,25 +4,22 @@ class Solution {
         HashMap<Character, Integer> hm = new HashMap<>();
         for(int i = 0;i<s.length();i++){
             char c = s.charAt(i);
-            if(hm.containsKey(c)){
-                hm.put(c, hm.get(c) + 1);
-            }else{
-                hm.put(c,1);
-            }
+            hm.put(c, hm.getOrDefault(c, 0) + 1);
+            
         }
         for(int i = 0;i<t.length();i++){
             char c = t.charAt(i);
+            
             if(hm.containsKey(c) == false){
                 return false;
+            }else if(hm.get(c) == 1){
+                hm.remove(c);
             }else{
                  hm.put(c, hm.get(c) - 1);
-                if(hm.get(c) < 0){
-                    return false;
-                }
-               
             }
         }
-        return true;
+        
+        return hm.size() == 0;
         
     }
 }
