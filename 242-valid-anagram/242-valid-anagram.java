26bd99f61d51d80089a5210aa77d0d78c6e9ea9b
Nitin Pandey <73239889+Nitinp13@users.noreply.github.com>
2022-08-06 21:01:45 +0530

    Time: 6 ms (60.23%), Space: 43.7 MB (62.79%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,5 +1,5 @@
 class Solution {
-    public boolean isAnagram(String s, String t) {
+     public boolean isAnagram(String s, String t) {
         int[] alp = new int[26];
     for (int i = 0; i < s.length(); i++) 
         alp[s.charAt(i) - 'a']++;
