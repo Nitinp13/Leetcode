41286e3951fa011114d8e152b415f1417957675b
Nitin Pandey <73239889+Nitinp13@users.noreply.github.com>
2022-08-09 21:06:30 +0530

    Time: 2 ms (99.95%), Space: 43.5 MB (85.00%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,33 +1,62 @@
 class Solution {
-     public int lengthOfLIS(int[] nums) {
-        ArrayList<Integer> arr = new ArrayList<>();
-        arr.add(nums[0]);
-         
-        for(int i = 1; i < nums.length;i++){
-            if(nums[i] > arr.get(arr.size() - 1)){
-                arr.add(nums[i]);
-            }else{
-                int ind = lowerbound(arr, nums[i]);
-                arr.set(ind, nums[i]);
-            }
-        
-        }
-    return arr.size();
-}
-    public int lowerbound(ArrayList<Integer> nums, int key){
-        int low = 0, high = nums.size();
-        int mid;
-        while(low < high){
-            mid = low + (high - low)/2;
-            
-            if(key <= nums.get(mid)){
-                high = mid;
-            }else{
-                low = mid + 1;
+     // better soln 1ms
+    public int lengthOfLIS(int[] nums) {
+        int[] sub = new int[nums.length];
+        sub[0] = nums[0];
+        int currentIndex = 0;
+        int maxLength = currentIndex;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > sub[currentIndex]) {
+                sub[++currentIndex] = nums[i];
+                maxLength = Math.max(maxLength, currentIndex);
+            } else {
+                int j = 0;
+                while (sub[j] < nums[i]) {
+                    j++;
+                }
+                sub[j] = nums[i];
             }
         }
-        return low;
+        return maxLength + 1;
     }
+    
+    
+    
+    // Normal Soln
+//      public int lengthOfLIS(int[] nums) {
+//         
+         
+//          
+//         ArrayList<Integer> arr = new ArrayList<>();
+//         arr.add(nums[0]);
+         
+//         for(int i = 1; i < nums.length;i++){
+//             if(nums[i] > arr.get(arr.size() - 1)){
+//                 arr.add(nums[i]);
+//             }else{
+//                 int ind = lowerbound(arr, nums[i]);
+//                 arr.set(ind, nums[i]);
+//             }
+        
+//         }
+//     return arr.size();
+// }
+//     public int lowerbound(ArrayList<Integer> nums, int key){
+//         int low = 0, high = nums.size();
+//         int mid;
+//         while(low < high){
+//             mid = low + (high - low)/2;
+            
+//             if(key <= nums.get(mid)){
+//                 high = mid;
+//             }else{
+//                 low = mid + 1;
+//             }
+//         }
+//         return low;
+//     }
+    
+    // Got Error
     
 //     public int lis(int ind, int prev, int[] nums,int n, ArrayList<ArrayList<Integer>> arr){
 //         if(ind == n) return 0;
