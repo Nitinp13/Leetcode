446113fd8b566a191f4b741d59cc9433e2bea11e
Nitin Pandey <73239889+Nitinp13@users.noreply.github.com>
2022-08-16 19:35:53 +0530

    Time: 2 ms (90.88%), Space: 42.6 MB (13.15%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -30,9 +30,7 @@ class Solution {
         
 //         return st.isEmpty();
 //     }
-    //Short soln 3ms
-    // Note if you dont use stack and use character array then the complexity is 0ms
-    public boolean isValid(String s) {
+  public boolean isValid(String s) {
 	Stack<Character> stack = new Stack<Character>();
 	for (char c : s.toCharArray()) {
 		if (c == '(')
@@ -45,5 +43,5 @@ class Solution {
 			return false;
 	}
 	return stack.isEmpty();
-}
+}   
 }
