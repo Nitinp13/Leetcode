d3e0de43185e66ec55a3c3e38fad62397cef7ba3
Nitin Pandey <73239889+Nitinp13@users.noreply.github.com>
2022-08-22 21:27:30 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (56.90%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -2,6 +2,8 @@ class Solution {
       //   public int rob(int[] nums) {
       // }
     
+    // Memoirization 1 ms 
+    // TC -> O(N)   SC - > O(N) + O(N) 
      public int rob(int[] nums) {
          int[] dp = new int[nums.length];
          Arrays.fill(dp, -1);
@@ -20,7 +22,7 @@ class Solution {
         return dp[index] = Math.max(pick, notPick);
     }
     
-    //Normal Recursion TLE  T = 2*n
+    //Normal Recursion TLE  TC -> O(2*N)
 //     public int rob(int[] nums) {
 //        return robRec(nums, nums.length-1);
           
