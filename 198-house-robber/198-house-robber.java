b18c0198f9ac48f19a326dc2ce7763ae3ee66f6e
Nitin Pandey <73239889+Nitinp13@users.noreply.github.com>
2022-08-22 21:46:55 +0530

    Time: 1 ms (20.02%), Space: 41.3 MB (65.02%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -2,25 +2,51 @@ class Solution {
       //   public int rob(int[] nums) {
       // }
     
-    // Memoirization 1 ms 
-    // TC -> O(N)   SC - > O(N) + O(N) 
-     public int rob(int[] nums) {
+    public int rob(int[] nums) {
          int[] dp = new int[nums.length];
          Arrays.fill(dp, -1);
-       return robRec(nums,dp, nums.length-1);
+       return robRec(nums,dp);
           
     } 
     
-    public int robRec(int[] a, int[] dp,  int index){
-        if(index == 0) return a[index];
-        if(index < 0) return 0;
-        if(dp[index] != -1) return dp[index];
+    public int robRec(int[] a, int[] dp){
+        dp[0] = a[0];
+        int res = 0;
+        int n = a.length;
+        if(n < 2) return a[0];
         
-        int pick = a[index] + robRec(a,dp, index - 2);
-        int notPick = 0 + robRec(a,dp, index-1);
-        
-        return dp[index] = Math.max(pick, notPick);
+        for(int i = 1;i<n;i++){
+            int take = a[i];
+            if(i > 1){
+                take += dp[i-2];
+            } 
+            int notTake = 0 + dp[i-1];
+            
+            dp[i] = Math.max(take, notTake);   
+            res = Math.max(res, dp[i]);
+        }
+        return res;
     }
+    
+    // Memoization 0 ms  (Top - Down Approach) 
+    // TC -> O(N)   SC - > O(N) + O(N) 
+//      public int rob(int[] nums) {
+//          int[] dp = new int[nums.length];
+//          Arrays.fill(dp, -1);
+//        return robRec(nums,dp, nums.length-1);
+          
+//     } 
+    
+//     public int robRec(int[] a, int[] dp,  int index){
+//         if(index == 0) return a[index];
+//         if(index < 0) return 0;
+//         if(dp[index] != -1) return dp[index];
+        
+//         int pick = a[index] + robRec(a,dp, index - 2);
+//         int notPick = 0 + robRec(a,dp, index-1);
+        
+//         return dp[index] = Math.max(pick, notPick);
+//     }
     
     //Normal Recursion TLE  TC -> O(2*N)
 //     public int rob(int[] nums) {
