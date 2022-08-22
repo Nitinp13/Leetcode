2cdf9e636e72fd71ba2f25a990c7f52f46c0666b
Nitin Pandey <73239889+Nitinp13@users.noreply.github.com>
2022-08-22 21:55:03 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (56.90%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,33 +1,60 @@
 class Solution {
-      //   public int rob(int[] nums) {
-      // }
-    // Tabulation 1 ms  (Bottom - Up Approach) 
-    // TC -> O(N)   SC - > O(N) 
-    public int rob(int[] nums) {
-         int[] dp = new int[nums.length];
+      // Tabulation with Space Optimization 1 ms  (Bottom - Up Approach) 
+    // TC -> O(N)   SC - > O(1) 
+    public int rob(int[] a) {
+         int[] dp = new int[a.length];
          Arrays.fill(dp, -1);
-       return robRec(nums,dp);
-          
-    } 
-    
-    public int robRec(int[] a, int[] dp){
-        dp[0] = a[0];
-        int res = 0;
+        
+         int prev = a[0];
+        int prev2 = 0;
         int n = a.length;
         if(n < 2) return a[0];
         
         for(int i = 1;i<n;i++){
             int take = a[i];
             if(i > 1){
-                take += dp[i-2];
+                take += prev2;
             } 
-            int notTake = 0 + dp[i-1];
+            int notTake = 0 + prev;
             
-            dp[i] = Math.max(take, notTake);   
-            res = Math.max(res, dp[i]);
+            int curr = Math.max(take, notTake);   
+            prev2 = prev;
+            prev = curr;
         }
-        return res;
-    }
+        return prev;
+          
+    } 
+    
+    
+    
+    
+    // Tabulation 1 ms  (Bottom - Up Approach) 
+    // TC -> O(N)   SC - > O(N) 
+//     public int rob(int[] nums) {
+//          int[] dp = new int[nums.length];
+//          Arrays.fill(dp, -1);
+//        return robRec(nums,dp);
+          
+//     } 
+    
+//     public int robRec(int[] a, int[] dp){
+//         dp[0] = a[0];
+//         int res = 0;
+//         int n = a.length;
+//         if(n < 2) return a[0];
+        
+//         for(int i = 1;i<n;i++){
+//             int take = a[i];
+//             if(i > 1){
+//                 take += dp[i-2];
+//             } 
+//             int notTake = 0 + dp[i-1];
+            
+//             dp[i] = Math.max(take, notTake);   
+//             res = Math.max(res, dp[i]);
+//         }
+//         return res;
+//     }
     
     // Memoization 0 ms  (Top - Down Approach) 
     // TC -> O(N)   SC - > O(N) + O(N) 
