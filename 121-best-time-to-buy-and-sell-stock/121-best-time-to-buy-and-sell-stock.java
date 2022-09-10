81d7d27215eba9fec1707c2549340ba705c5c2b8
Nitin Pandey <73239889+Nitinp13@users.noreply.github.com>
2022-09-10 20:43:44 +0530

    Time: 3 ms (69.71%), Space: 85 MB (7.61%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -1,13 +1,29 @@
 class Solution {
-    public int maxProfit(int[] prices) {
+    // from behind checking which value generates maximum profit while subtracting each with max val 
+//     public int maxProfit(int[] prices) {
+//         //int res = 0;
+//         int max = prices[prices.length-1];
+//         int prof = 0;
+        
+//         for(int i = prices.length-2;i>=0;i--){
+//             int p = prices[i];
+//             prof = Math.max(prof, max - p);
+//             max = Math.max(max, p);
+            
+//         }
+//         return prof;
+//     }
+    
+    //from starting check which value generates max profits by subtracting least value so far from it
+     public int maxProfit(int[] prices) {
         //int res = 0;
-        int max = prices[prices.length-1];
+        int min = prices[0];
         int prof = 0;
         
-        for(int i = prices.length-2;i>=0;i--){
+        for(int i = 1;i< prices.length;i++){
             int p = prices[i];
-            prof = Math.max(prof, max - p);
-            max = Math.max(max, p);
+            prof = Math.max(prof, p-min);
+            min = Math.min(min, p);
             
         }
         return prof;
