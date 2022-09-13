5d7c511d76e0e0a663a06d129f0303f7fa1a1bcd
Nitin Pandey <73239889+Nitinp13@users.noreply.github.com>
2022-09-13 21:55:55 +0530

    Time: 4 ms (38.86%), Space: 47.8 MB (58.81%) - LeetHub

--- 393-utf-8-validation/393-utf-8-validation.java
+++ 393-utf-8-validation/393-utf-8-validation.java
@@ -6,13 +6,13 @@ class Solution {
         for(int val : data){
             
             if(rbytes == 0){
-                 if((val >> 7) == 0b0){
+                 if((val >> 7) == 0b0){// for 1 byte
                      rbytes = 0;
-                 }else if((val >> 5) == 0b110){
+                 }else if((val >> 5) == 0b110){// for 2 byte
                      rbytes = 1;
-                 }else if((val >> 4) == 0b1110){
+                 }else if((val >> 4) == 0b1110){// for 3 byte
                      rbytes = 2;
-                 }else if((val >> 3) == 0b11110){
+                 }else if((val >> 3) == 0b11110){// for 4 byte
                      rbytes = 3;
                  }else{
                      return false;
